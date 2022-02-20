5191da5989c411d1443fb6263a7d90dacae55620
Dong Zeng <zeng-dong@hotmail.com>
2022-02-20 12:36:06 -0600

    tried sonarlint

--- WebApiFundamentals/CityInfo.Api/Program.cs
+++ WebApiFundamentals/CityInfo.Api/Program.cs
@@ -6,11 +6,19 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+#pragma warning disable S1135 // Track uses of "TODO" tags
+// TODO: abc
+
 if (app.Environment.IsDevelopment())
+#pragma warning restore S1135 // Track uses of "TODO" tags
 {
+    //this is comment
     app.UseSwagger();
-    app.UseSwaggerUI();
+
+#pragma warning disable S125 // Sections of code should not be commented out
+    //app.UseSwaggerUI();
 }
+#pragma warning restore S125 // Sections of code should not be commented out
 
 app.UseHttpsRedirection();
 
