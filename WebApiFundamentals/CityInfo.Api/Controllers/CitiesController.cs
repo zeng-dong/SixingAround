b67e45de0bef32c428d0f0c7485a4009ec306058
Dong Zeng <zeng-dong@hotmail.com>
2022-02-22 08:59:54 -0600

    adding models and in memory data store

--- WebApiFundamentals/CityInfo.Api/Controllers/CitiesController.cs
+++ WebApiFundamentals/CityInfo.Api/Controllers/CitiesController.cs
@@ -9,11 +9,6 @@ public class CitiesController : ControllerBase
     [HttpGet]
     public JsonResult GetCities()
     {
-        return new JsonResult(
-            new List<object>
-            {
-                new { id = 1, name = "New York City" },
-                new { id = 2, name = "Houston "}
-            });
+        return new JsonResult(CitiesDataStore.Current);
     }
 }
