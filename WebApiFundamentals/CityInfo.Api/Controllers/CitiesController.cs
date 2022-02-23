7c1990e3a771a616eda5515f78cd40f3571a5d5f
Dong Zeng <zeng-dong@hotmail.com>
2022-02-23 09:37:45 -0600

    returning city

--- WebApiFundamentals/CityInfo.Api/Controllers/CitiesController.cs
+++ WebApiFundamentals/CityInfo.Api/Controllers/CitiesController.cs
@@ -11,4 +11,20 @@ public class CitiesController : ControllerBase
     {
         return new JsonResult(CitiesDataStore.Current);
     }
+
+    /// <summary>
+    /// Get a city by id
+    /// </summary>
+    /// <param name="id">The id of the city to get</param>
+    /// <param name="includePointsOfInterest">Whether or not to include the points of interest</param>
+    /// <returns>An IActionResult</returns>
+    /// <response code="200">Returns the requested city</response>
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetCity(int id)
+    {
+        return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id));
+    }
 }
