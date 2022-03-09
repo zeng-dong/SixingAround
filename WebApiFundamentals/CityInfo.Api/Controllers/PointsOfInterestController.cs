15430b46342db6436160a731c55d7cb42bbd9ead
Dong Zeng <zeng-dong@hotmail.com>
2022-03-09 08:47:40 -0600

    initial PointOfInterestController

---
+++ WebApiFundamentals/CityInfo.Api/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,43 @@
+﻿using CityInfo.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers;
+
+[Route("api/cities/{cityId}/pointsofinterest")]
+[ApiController]
+public class PointsOfInterestController : ControllerBase
+{
+    private readonly ILogger<PointsOfInterestController> _logger;
+
+    public PointsOfInterestController(ILogger<PointsOfInterestController> logger
+        )
+    {
+        _logger = logger ??
+            throw new ArgumentNullException(nameof(logger));
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(
+        int cityId)
+    {
+        var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+
+        if (city == null) return NotFound();
+
+        return Ok(city.PointsOfInterest);
+    }
+
+    [HttpGet("{pointofinterestid}", Name = "GetPointOfInterest")]
+    public ActionResult<PointOfInterestDto> GetPointOfInterest(
+        int cityId, int pointOfInterestId)
+    {
+        var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+        if (city == null) return NotFound();
+
+        var pointOfInterest = city.PointsOfInterest.FirstOrDefault(c => c.Id == pointOfInterestId);
+        if (pointOfInterest == null) return NotFound();
+
+        return Ok(pointOfInterest);
+    }
+}
