d14d1944c69d0e2d91fd04ad1e0047386abd374b
Dong Zeng <zeng-dong@hotmail.com>
2022-03-09 08:57:14 -0600

    setting up xml content negotiation

--- WebApiFundamentals/CityInfo.Api/Program.cs
+++ WebApiFundamentals/CityInfo.Api/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+})
+    .AddXmlDataContractSerializerFormatters();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
