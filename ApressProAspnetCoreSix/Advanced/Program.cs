508f0f1cd599e2c330ae825e5f3b4f841fe4f902
Dong Zeng <zeng-dong@hotmail.com>
2022-03-24 08:55:22 -0500

    adding migration Initial

--- ApressProAspnetCoreSix/Advanced/Program.cs
+++ ApressProAspnetCoreSix/Advanced/Program.cs
@@ -1,6 +1,19 @@
+using Advanced.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:PeopleConnection"]);
+    opts.EnableSensitiveDataLogging(true);
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
+SeedData.SeedDatabase(context);
 
 app.Run();
