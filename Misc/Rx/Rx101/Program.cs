283c57caa6ee98a7f02efdb0c817cf39a6a792a2
Dong Zeng <zeng-dong@hotmail.com>
2022-03-27 19:49:51 -0500

    starting trying rx

---
+++ Misc/Rx/Rx101/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Reactive.Linq;
+
+var query = from number in Enumerable.Range(1, 10) select number;
+var oq = query.ToObservable();
+
+oq.Subscribe(Console.WriteLine, () => { Console.WriteLine("I am done."); });
