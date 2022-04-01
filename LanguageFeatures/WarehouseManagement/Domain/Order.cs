510170e7fec4b9a2d7fcbaffa0852b455a1a2dd8
Dong Zeng <zeng-dong@hotmail.com>
2022-04-01 09:49:44 -0500

    warehouseMan to host language features

---
+++ LanguageFeatures/WarehouseManagement/Domain/Order.cs
@@ -0,0 +1,22 @@
+﻿namespace WarehouseManagement.Domain;
+
+public class Order
+{
+    public Guid OrderNumber { get; init; }
+    public ShippingProvider ShippingProvider { get; init; }
+    public int Total { get; }
+    public bool IsReadyForShipment { get; set; } = true;
+    public IEnumerable<Item> LineItems { get; set; }
+
+    public Order()
+    {
+        OrderNumber = Guid.NewGuid();
+    }
+}
+
+public class Item
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public bool InStock { get; set; }
+}
---
+++ LanguageFeatures/WarehouseManagement/Domain/ShippingProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace WarehouseManagement.Domain;
+
+public class ShippingProvider
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public decimal FreightCost { get; set; }
+}
