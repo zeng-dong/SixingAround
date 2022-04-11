ec44b3313ffceb238d4172872f0319f5906b3d70
Dong Zeng <zeng-dong@hotmail.com>
2022-04-11 13:39:08 -0500

    setup orderapi

---
+++ Microservices/DistributedTracing/OrderAPI/Controllers/OrdersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrdersController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get()
+    {
+        return Ok("Hello Order");
+    }
+}
