7d112739f0510d9517da5f352f9efceda12da772
Dong Zeng <zeng-dong@hotmail.com>
2022-04-12 12:18:33 -0500

    basic instrumentation

--- Microservices/DistributedTracing/BabySteps/Program.cs
+++ Microservices/DistributedTracing/BabySteps/Program.cs
@@ -1,30 +1,46 @@
 ﻿using OpenTelemetry;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+namespace BabySteps;
 
-using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("MySample"))
-                .AddSource("Sample.DistributedTracing")
-                .AddConsoleExporter()
-                .Build();
-
-await DoSomeWork("banana", 8);
-Console.WriteLine("Example work done");
-
-static async Task DoSomeWork(string foo, int bar)
+internal class Program
 {
-    await StepOne();
-    await StepTwo();
-}
+    static readonly ActivitySource source = new ActivitySource("Give_it_a_unique_source_name_aka_tracer_name", "1.0.0");
 
-static async Task StepOne()
-{
-    await Task.Delay(500);
-}
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
-static async Task StepTwo()
-{
-    await Task.Delay(1000);
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("MySample"))
+                        .AddSource("Give_it_a_unique_source_name_aka_tracer_name")
+                        .AddConsoleExporter()
+                        .Build();
+
+        await DoSomeWork("banana", 8);
+        Console.WriteLine("Example work done");
+    }
+
+    static async Task DoSomeWork(string foo, int bar)
+    {
+        using (Activity activity = source?.StartActivity("DoSomeWork")!)
+        {
+            if (activity == null) Console.WriteLine("activity is null");
+
+            await StepOne();
+            await StepTwo();
+        }
+    }
+
+    static async Task StepOne()
+    {
+        await Task.Delay(500);
+    }
+
+    static async Task StepTwo()
+    {
+        await Task.Delay(1000);
+    }
 }
