dcfb5252539a44813d2eea91d469562f4da7f5b6
Dong Zeng <zeng-dong@hotmail.com>
2022-04-12 11:17:42 -0500

    simple console to do step by step opentelemetry

---
+++ Microservices/DistributedTracing/BabySteps/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+Console.WriteLine("Hello, World!");
+
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("MySample"))
+                .AddSource("Sample.DistributedTracing")
+                .AddConsoleExporter()
+                .Build();
+
+await DoSomeWork("banana", 8);
+Console.WriteLine("Example work done");
+
+static async Task DoSomeWork(string foo, int bar)
+{
+    await StepOne();
+    await StepTwo();
+}
+
+static async Task StepOne()
+{
+    await Task.Delay(500);
+}
+
+static async Task StepTwo()
+{
+    await Task.Delay(1000);
+}
