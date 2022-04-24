20555d9b1cd15dcf2476f314c6a958eaf4ca6ce3
Dong Zeng <zeng-dong@hotmail.com>
2022-04-24 13:54:59 -0500

    shopping cart events

---
+++ Poland/EventSourcing/SelfPacedEventSourcing/DomainEvents/ShoppingCartEvents.cs
@@ -0,0 +1,36 @@
+﻿namespace SelfPacedEventSourcing.DomainEvents;
+public record ShoppingCartOpened
+(
+    Guid ShoppingCartId,
+    Guid ClientId
+);
+
+public record ProductItemAddedToShoppingCart(
+    Guid ShoppingCartId,
+    PricedProductItem ProductItem
+);
+
+public record ProductItemRemovedFromShoppingCart(
+    Guid ShoppingCartId,
+    PricedProductItem ProductItem
+);
+
+public record ShoppingCartConfirmed(
+    Guid ShoppingCartId,
+    DateTime ConfirmedAt
+);
+
+public record ShoppingCartCanceled(
+    Guid ShoppingCartId,
+    DateTime CanceledAt
+);
+
+public class PricedProductItem
+{
+    public Guid ProductId { get; set; }
+    public decimal UnitPrice { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal TotalPrice => Quantity * UnitPrice;
+}
