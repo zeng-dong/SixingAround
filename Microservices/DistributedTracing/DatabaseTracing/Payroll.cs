1c228843b75dd0c05e39c719785c9ea13debdb91
Dong Zeng <zeng-dong@hotmail.com>
2022-05-09 20:20:26 -0500

    tidy up

---
+++ Microservices/DistributedTracing/DatabaseTracing/Payroll.cs
@@ -0,0 +1,5 @@
+﻿public class Payroll
+{
+    public int EmployeeId { get; set; }
+    public decimal PayRateInUSD { get; set; }
+}
---
+++ Microservices/DistributedTracing/DatabaseTracing/Timekeeping.cs
@@ -0,0 +1,7 @@
+﻿public class Timekeeping
+{
+    public int EmployeeId { get; set; }
+    public int ProjectId { get; set; }
+    public DateTime WeekClosingDate { get; set; }
+    public int HoursWorked { get; set; }
+}
