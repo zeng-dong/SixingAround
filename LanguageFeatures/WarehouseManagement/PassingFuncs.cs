7d7fcf3af8e3263611109cd6001a1042ac2d1bf0
Dong Zeng <zeng-dong@hotmail.com>
2022-08-16 16:05:25 -0500

    repeatedly

--- LanguageFeatures/WarehouseManagement/PassingFuncs.cs
+++ LanguageFeatures/WarehouseManagement/PassingFuncs.cs
@@ -4,7 +4,17 @@ internal static class PassingFuncs
 {
     private static int Add(int x) => x + x;
 
+    private static int Add(Func<int, int> how, int original) => how(original) + how(original);
+
     private static int Time(int x) => x * x;
+
+    ///  how can we do func of func:
+    ///private static int Strange(Func<Func<int, int>, int> how, int original)
+    ///{
+    ///    ///var x = how(original);
+    ///
+    ///    return how(Add, original);
+    ///}
 
     private static int Execute(Func<int, int> how, int original) => how(original);
 
@@ -14,5 +24,8 @@ internal static class PassingFuncs
         Console.WriteLine(Execute(Time, 3));
 
         Console.WriteLine(Execute(Time, Execute(Add, 2)));
+
+        Console.WriteLine(Add(Add, 3));
+        Console.WriteLine(Add(Time, 3));
     }
 }
