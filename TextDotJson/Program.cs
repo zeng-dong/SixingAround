a6a0efab5b3c660a6d4500e70d56a233e5ef7a22
Dong Zeng <zeng-dong@hotmail.com>
2023-03-08 20:49:05 -0600

    generic

--- TextDotJson/Program.cs
+++ TextDotJson/Program.cs
@@ -1,20 +1,9 @@
-﻿using System.Text.Json;
-
-namespace TextDotJson;
+﻿namespace TextDotJson;
 
 internal static class Program
 {
     static void Main(string[] args)
     {
-        var weatherForecast = new WeatherForecast
-        {
-            Date = DateTime.Parse("2021-12-01"),
-            TemperatureCelsius = 25,
-            Summary = "Hot"
-        };
-
-        string jsonString = JsonSerializer.Serialize(weatherForecast);
-
-        Console.WriteLine(jsonString);
+        SimpleSerializer.SimpleSerialize();
     }
 }
---
+++ TextDotJson/SimpleSerializer.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+
+namespace TextDotJson;
+
+internal static class SimpleSerializer
+{
+    internal static void SimpleSerialize()
+    {
+        var weatherForecast = new WeatherForecast
+        {
+            Date = DateTime.Parse("2021-12-01"),
+            TemperatureCelsius = 25,
+            Summary = "Hot"
+        };
+
+        string jsonString = JsonSerializer.Serialize(weatherForecast);
+        Console.WriteLine(jsonString);
+        Console.Clear();
+
+        // generic
+        string jsonStringGenerics = JsonSerializer.Serialize<WeatherForecast>(weatherForecast);
+        Console.WriteLine(jsonStringGenerics);
+        Console.Clear();
+
+        //3. Serialization to a file
+        string fileName = "WeatherForecast.json";
+        File.WriteAllText(fileName, jsonString);
+    }
+}
